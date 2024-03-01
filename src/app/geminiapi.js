947eb921796aeb3ec647ddb914e33834a026c46e
Erick Guerra <eig27@txstate.edu>
2024-03-01 16:41:57 -0600

    used dotenv to store api key and get Gemini api working

--- src/app/geminiapi.js
+++ src/app/geminiapi.js
@@ -1,13 +1,15 @@
+require('dotenv').config();
+
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 // Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 async function run() {
   // For text-only input, use the gemini-pro model
   const model = genAI.getGenerativeModel({ model: "gemini-pro"});
 
-  const prompt = "Write a story about a magic backpack."
+  const prompt = "tell me a joke about a cat"
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
--- src/app/page.js
+++ src/app/page.js
@@ -24,6 +24,7 @@ function MyForm() {
   const handleSubmit = (event) =>{
     event.preventDefault();
     alert(request)
+    
   }
 
   return (
