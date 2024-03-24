4fa3e0ce47a9d291834408212e1c65970053f600
Erick Guerra <eig27@txstate.edu>
2024-03-23 22:18:36 -0500

    added helpful comments to flowchart constructor

--- src/app/page.js
+++ src/app/page.js
@@ -84,37 +84,37 @@ function MyForm() {
 
 
   async function FlowChartConstructor(ArrOfNodes){
-    let nodeArr = ArrOfNodes;
-    const nodeMap = new Map();
+    let nodeArr = ArrOfNodes; //array containing all album node objects
+    const nodeMap = new Map(); //map containing albums to check if specific album node already exists
     let idCount = 1;
     let source = "";
     let target = "";
-  
+    //loops through the array of album node objects to create corresponding ReactFlow nodes and edges
     for (let i = 1; i < nodeArr.length; i++){
-      source = idCount.toString();
+      source = idCount.toString(); //source starts at the first node
        
       if ((nodeMap.has(nodeArr[i].albumName)) == false){
-        
+        //this code excecutes because we reached an album we havent created a node for yet
         const node = {id: idCount.toString(), position: { x: 100, y: nodeMap.size * 100 }, data: { label: nodeArr[i].albumName} }
-        nodeMap.set(nodeArr[i].albumName, idCount);
+        nodeMap.set(nodeArr[i].albumName, idCount); //add to map for future reference
         console.log("ADDING NODE " + nodeArr[i].albumName + " SINCE NODE DOESNT ALREADY EXIST");
-        setNodes((nds) => nds.concat(node));
+        setNodes((nds) => nds.concat(node)); //adding album as a ReactFlow node
         idCount = idCount + 1;
-
-        const edgeCount = nodeArr[i].edgeCount;
+        //now we loop through and deal with the edges of this node we found
+        const edgeCount = nodeArr[i].edgeCount; //finds the edge count of the node we are on
         for (let j=1; j <= edgeCount;j++){
           let stringName = "edgeAlbum" + j;
           let edgeName = "edgeDesc" + j;
           const edgeDesc = nodeArr[i][edgeName] 
           const edgeNode = nodeArr[i][stringName];
-          if (nodeMap.has(edgeNode) == false){
-            target = idCount.toString();
-            nodeMap.set(edgeNode, idCount);
+          if (nodeMap.has(edgeNode) == false){ //making sure the edge album is not an album already found
+            target = idCount.toString(); 
+            nodeMap.set(edgeNode, idCount); //adds the edge album to node map
             const newNode = {id: idCount.toString(), position: { x: j*100, y: nodeMap.size * 100}, data: { label: edgeNode} }
             console.log("ADDING NODE " + edgeNode + " AS AN EDGE NODE")
-            setNodes((nds) => nds.concat(newNode));
+            setNodes((nds) => nds.concat(newNode)); //adds edge album as a ReactFlow node since not already created
             idCount = idCount + 1;
-            const edgeId = 'e' + source + '-' + target;
+            const edgeId = 'e' + source + '-' + target; //creates id for current edge
             const edge = {id: edgeId, source: source, target: target, markerEnd: {
               type: MarkerType.ArrowClosed,
               width: 20,
@@ -125,9 +125,9 @@ function MyForm() {
               stroke: '#FF0072',
             }, }
             console.log("ADDING EDGE FROM " + source + " TO " + target + " SINCE NEW NODE")
-            setEdges((eds) => eds.concat(edge));
-          }else{
-            target = nodeMap.get(edgeNode).toString();
+            setEdges((eds) => eds.concat(edge)); //adds edge as a ReactFlow edge
+          }else{ //code below excecutes when a node already created for an edge album is found
+            target = nodeMap.get(edgeNode).toString(); //finds the node id of this album
             const edgeId = 'e' + source + '-' + target;
             const edge = {id: edgeId, source: source, target: target, markerEnd: {
               type: MarkerType.ArrowClosed,
@@ -139,24 +139,24 @@ function MyForm() {
               stroke: '#FF0072',
             },  }
             console.log("ADDING EDGE FROM " + source + " TO " + target + " SINCE ALREADY EXISTING NODE")
-            setEdges((eds) => eds.concat(edge));
+            setEdges((eds) => eds.concat(edge)); //adds edge as a ReactFlow edge
           }
         }
-      }else{
-        let sourceID = nodeMap.get(nodeArr[i].albumName);
+      }else{//code excecutes when album is found that is already stored in the node map
+        let sourceID = nodeMap.get(nodeArr[i].albumName); //gets already existing id of this album
         source = sourceID.toString();
         const edgeCount = nodeArr[i].edgeCount;
-        for (let j=1; j <= edgeCount;j++){
+        for (let j=1; j <= edgeCount;j++){ //iterate the edges of this album
           let stringName = "edgeAlbum" + j;
           let edgeName = "edgeDesc" + j;
           const edgeDesc = nodeArr[i][edgeName] 
           const edgeNode = nodeArr[i][stringName];
-          if (nodeMap.has(edgeNode) == false){
+          if (nodeMap.has(edgeNode) == false){ //checks if edge album doesn't already exist in node map
             target = idCount.toString();
             nodeMap.set(edgeNode, idCount);
             const newNode = {id: idCount.toString(), position: { x: j*100, y: nodeMap.size * 100 }, data: { label: edgeNode} }
             console.log("ADDING NODE " + edgeNode + " AS AN EDGE NODE")
-            setNodes((nds) => nds.concat(newNode));
+            setNodes((nds) => nds.concat(newNode)); //adds edge album as a ReactFlow node
             idCount = idCount + 1;
             const edgeId = 'e' + source + '-' + target;
             const edge = {id: edgeId, source: source, target: target, markerEnd: {
@@ -169,9 +169,9 @@ function MyForm() {
               stroke: '#FF0072',
             },  }
             console.log("ADDING EDGE FROM " + source + " TO " + target + " SINCE NEW NODE")
-            setEdges((eds) => eds.concat(edge));
-          }else{
-            target = nodeMap.get(edgeNode).toString();
+            setEdges((eds) => eds.concat(edge)); //adds edge as ReactFlow edge
+          }else{ //edge album node already exists so target will be found in the map
+            target = nodeMap.get(edgeNode).toString(); //gets target id from node map
             const edgeId = 'e' + source + '-' + target;
             const edge = {id: edgeId, source: source, target: target, markerEnd: {
               type: MarkerType.ArrowClosed,
@@ -183,7 +183,7 @@ function MyForm() {
               stroke: '#FF0072',
             },  }
             console.log("ADDING EDGE FROM " + source + " TO " + target + " SINCE ALREADY EXISTING NODE")
-            setEdges((eds) => eds.concat(edge));
+            setEdges((eds) => eds.concat(edge)); //adds edge as ReactFlow edge
           }
         }
       }
@@ -195,7 +195,6 @@ function MyForm() {
     event.preventDefault();
     (async () => {
       const flowChartArr = await RunGemini(request)
-      setResponseData(flowChartArr[1].albumName);
       console.log(flowChartArr.length + "LENGTH OF THE ARRAY CONTAINING NODE OBJECTS INSIDE THE FUNC");
       await FlowChartConstructor(flowChartArr);
     })()
